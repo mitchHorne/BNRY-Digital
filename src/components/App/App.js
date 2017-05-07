b63e899ad848e4365012e7cc752ed9b8702e0424
hornemitch <mitchhorne92@gmail.com>
2017-05-07 14:56:12 +0200

    Remove unnncessary css import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Slider from '../Slider/Slider';
 
 class App extends Component {
@@ -7,10 +6,18 @@ class App extends Component {
     return (
       <div className="App">
         <Slider duration={1000}>
-          <div background='blue'></div>
-          <div background='red'></div>
-          <div background='yellow'></div >
-          <div background='teal'></div >
+          <div background='blue'>
+            <h1>I AM SLIDE !!</h1>
+          </div>
+          <div background='red'>
+            <h1>I AM SLIDE 2!</h1>
+          </div>
+          <div background='yellow'>
+            <h1>I AM SLIDE 3!</h1>
+          </div >
+          <div background='teal'>
+            <h1>I AM SLIDE 4!</h1>
+          </div >
         </Slider >
       </div >
     );
